fe01317ade104f3c759beedd90ffd7438450f915
maor <maorgithub@gmail.com>
2023-08-25 23:49:28 +0300

    Moving Ghosts
    # Description
    * update Enemy class with two new methods
    * local method #move in Enemy class handle the ghosts moving
    * local changeDirection in Enemy class handle ghosts collision and change his diretion

--- src/Enemy.js
+++ src/Enemy.js
@@ -12,11 +12,74 @@ export default class Enemy
         this.tileMap = tileMap;
 
         this.#loadImages();
+
+        this.movingDirection = Math.floor(Math.random() * Object.keys(MovingDirection).length);
+
+        this.directionTimerDefault = this.#random(1, 3);
+        this.directionTimer = this.directionTimerDefault;
     }
+
+    #random(min, max)
+    {
+        return Math.floor(Math.random() * (max - min + 1 ) ) + min;
+    }
+
     draw(context)
     {
+        this.#move();
+        this.#changeDirection();
+
         context.drawImage(this.image, this.x, this.y, this.tileSize, this.tileSize);
     }
+
+    #move()
+    {
+        if( !this.tileMap.didCollideWithEnvironment( this.x, this.y, this.movingDirection ) )
+        {
+            switch(this.movingDirection)
+            {
+                case MovingDirection.up:
+                    this.y -= this.velocity;
+                break;
+                
+                case MovingDirection.down:
+                    this.y += this.velocity;
+                break;
+                
+                case MovingDirection.left:
+                    this.x -= this.velocity;
+                break;
+                
+                case MovingDirection.right:
+                    this.x += this.velocity;
+                break;
+            }
+        }
+    }
+
+    #changeDirection()
+    {
+        this.directionTimer--;
+
+        let newMoveDirection = null;
+
+        if(this.directionTimer == 0)
+        {
+            this.directionTimer = this.directionTimerDefault;
+            newMoveDirection = Math.floor(Math.random() * Object.keys(MovingDirection).length );
+        };
+
+        if(newMoveDirection != null && this.movingDirection != newMoveDirection)
+        {
+            if(Number.isInteger( this.x / this.tileSize ) && Number.isInteger( this.y / this.tileSize ))
+            {
+                if( !this.tileMap.didCollideWithEnvironment(this.x , this.y , newMoveDirection) )
+                {
+                    this.movingDirection = newMoveDirection;
+                }
+            }
+        }
+    }   
 
     #loadImages()
     {
