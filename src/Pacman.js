2dbef12ce913227097b9689bbab9c2a0e6c7e9a7
maor <maorgithub@gmail.com>
2023-08-25 20:00:26 +0300

    Pacman moving
    #Description
    *add to pacman class method that handle pacman moving base on keycode

--- src/Pacman.js
+++ src/Pacman.js
@@ -9,12 +9,19 @@ export default class Pacman
         this.tileSize = tileSize;
         this.velocity = velocity;
         this.tileMap = tileMap;
+
+        this.currentMovingDirection = null;
+        this.requestMovingDirection = null;
+
+        document.addEventListener("keydown", this.#keydown);
+
         this.#loadPacmanImages();
 
     }
 
     draw(context) 
     {
+        this.#move();
         context.drawImage(
                 this.pacmanImages[this.pacmanImageIndex],
                 this.x,
@@ -46,6 +53,70 @@ export default class Pacman
             pacmanImage4
         ];
 
-        this.pacmanImageIndex = 2;
+        this.pacmanImageIndex = 0;
+    }
+
+    #keydown = (event) => {
+        // up
+        if(event.keyCode == 38 || event.keyCode == 87) 
+        {
+            if(this.currentMovingDirection == MovingDirection.down)
+
+                this.currentMovingDirection = MovingDirection.up;
+                this.requestMovingDirection = MovingDirection.up;
+        }
+        // down
+        if(event.keyCode == 40 ||  event.keyCode == 83) 
+        {
+            if(this.currentMovingDirection == MovingDirection.up)
+                this.currentMovingDirection = MovingDirection.down;
+                this.requestMovingDirection = MovingDirection.down;
+        }
+        // left
+        if(event.keyCode == 37 ||  event.keyCode == 65) 
+        {
+            if(this.currentMovingDirection == MovingDirection.right)
+                this.currentMovingDirection = MovingDirection.left;
+                this.requestMovingDirection = MovingDirection.left;
+        }
+        // right        
+        if(event.keyCode == 39 ||  event.keyCode == 68) 
+        {
+            if(this.currentMovingDirection == MovingDirection.left)
+                this.currentMovingDirection = MovingDirection.right;
+                this.requestMovingDirection = MovingDirection.right;
+        }
+
+    }
+
+    #move()
+    {
+        if(this.currentMovingDirection !== this.MovingDirection)
+        {
+            if(
+                Number.isInteger( this.x / this.tileSize ) && 
+                Number.isInteger(this.y / this.tileSize)
+                )
+                {
+                    this.currentMovingDirection = this.requestMovingDirection
+                }
+        }
+
+        switch(this.currentMovingDirection)
+        {
+            case MovingDirection.up:
+            this.y -= this.velocity;
+            break; 
+            case MovingDirection.down:
+            this.y += this.velocity;
+            break; 
+            case MovingDirection.left:
+            this.x -= this.velocity;
+            break; 
+            case MovingDirection.right:
+            this.x += this.velocity;
+            break; 
+        }
+       
     }
 } 
